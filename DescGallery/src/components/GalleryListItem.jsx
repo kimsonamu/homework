1ed0626e623909cf5d5578cb9f9e425bd3514a83
kimsonamu <twiggyramirz@gmail.com>
2024-05-18 05:04:04 +0900

    arrow add

--- DescGallery/src/components/GalleryListItem.jsx
+++ DescGallery/src/components/GalleryListItem.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
+import {SlArrowLeft} from "react-icons/sl";
 const GalleryListItem = ({ item,fSetImg,curData }) => {
     function fUdtImg(num=0) {
         fSetImg(num);
     }
     return (
         <>
-            <dt onClick={()=>fUdtImg(item.id-1)} className={item===curData?'on':''}>{item.title}</dt>
+            <dt onClick={()=>fUdtImg(item.id-1)} className={item===curData?'on':''}>{item.title}<SlArrowLeft /></dt>
             <dd className={item===curData?'on':''}>{item.desc}</dd>
         </>
     );
